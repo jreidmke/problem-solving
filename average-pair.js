4a5e15dd3c10cdc247642381f4f6010aa0a90e87
James Reid <jreidmke@gmail.com>
2021-02-02 12:58:46 -0600

    completed average-pair

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,20 @@
-// add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(arr, avg) {
+    if(!arr.length) return false;
+
+    let leftBound = 0;
+    let rightBound = arr.length - 1;
+
+    while(rightBound > leftBound) {
+        if((arr[leftBound] + arr[rightBound])/2 === avg) {
+            return true;
+        }  else if ((arr[leftBound] + arr[rightBound])/2 > avg) {
+            rightBound--
+        } 
+        else {
+            leftBound++
+        } 
+    };
+    return false; 
 }
+
+module.exports = averagePair;
