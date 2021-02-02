38a160078a42143dd2e9d303256f9768e09e874a
James Reid <jreidmke@gmail.com>
2021-02-02 13:06:21 -0600

    completed two-array-object

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,12 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
-}
+function twoArrayObject(keys, values) {
+    let obj = {}; 
+    let idx = 0;
+    for(let k of keys) {
+        obj[k] = values[idx] || null;
+        idx++;
+    }
+    return obj; 
+};
+
+module.exports = twoArrayObject; 
